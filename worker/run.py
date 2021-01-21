57a4db41d3a049ec8d59f2c8cc3a3853da845f67
Yusheng Wu <wuyusheng1023@gmail.com>
2021-01-21 23:27:02 +0200

    refactor: worker::run as entry, add if __name__...

--- worker/run.py
+++ worker/run.py
@@ -4,15 +4,15 @@ from interfaces.helpers import set_init_mock_redis
 from interfaces.nox_analyzer import NOxAnalyzer
 
 
-# def mock_worker():
-set_init_mock_redis()
-nox = NOxAnalyzer()
+def mock_worker():
+  set_init_mock_redis()
+  nox = NOxAnalyzer()
 
-while True:
-  log = nox.push_mock_data_to_redis()
-  print(log)
-  sleep(1)
+  while True:
+    log = nox.push_mock_data_to_redis()
+    print(log)
+    sleep(1)
 
 
-# if __name__ == '__main__':
-#   mock_worker()
+if __name__ == '__main__':
+  mock_worker()

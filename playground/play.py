febe00620a4326d9ba0dd6ca4490ee43cd9284e9
Yusheng Wu <wuyusheng1023@gmail.com>
2021-01-23 15:55:29 +0200

    DRF simple app runs in Docker container

--- playground/play.py
+++ playground/play.py
@@ -3,15 +3,20 @@ import json
 import time
 from datetime import datetime
 
-while True:
-  r = redis.Redis(host='localhost', port=6379, db=0)
-  list_len = r.llen('data')
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-  if list_len == 0:
-    print(f'{datetime.now()}: The list is empty\n')
-  else:
-    print(f'{datetime.now()}: The list length: {list_len}')
-    item = json.loads(r.rpop('data').decode('utf-8'))
-    print(f'Pop one item:\n{item}\n')
+while True:
   
+  try:
+    list_len = r.llen('data')
+
+    if list_len == 0:
+      print(f'{datetime.now()}: The list is empty\n')
+    else:
+      print(f'{datetime.now()}: The list length: {list_len}')
+      item = json.loads(r.rpop('data').decode('utf-8'))
+      print(f'Pop one item:\n{item}\n')
+  except:
+    print('Redis service disconnected.')
+
   time.sleep(2)

3e7aa91cf20694dabe62ea8c4155d1074d464efe
Yusheng Wu <wuyusheng1023@gmail.com>
2021-01-23 14:56:45 +0200

    pass: manual test by playground

--- playground/play.py
+++ playground/play.py
@@ -1,12 +1,17 @@
 import redis
 import json
 import time
+from datetime import datetime
 
 while True:
   r = redis.Redis(host='localhost', port=6379, db=0)
-  try:
-    print(json.loads(r.rpop('data').decode('utf-8')))
-    print(r.llen('data'))
-  except:
-    print('Now the list is empty')
-    time.sleep(10)
+  list_len = r.llen('data')
+
+  if list_len == 0:
+    print(f'{datetime.now()}: The list is empty\n')
+  else:
+    print(f'{datetime.now()}: The list length: {list_len}')
+    item = json.loads(r.rpop('data').decode('utf-8'))
+    print(f'Pop one item:\n{item}\n')
+  
+  time.sleep(0.1)
--- worker/run.py
+++ worker/run.py
@@ -1,17 +1,30 @@
+import os
 from time import sleep
 
-from interfaces.helpers import set_init_mock_redis
+import redis
+try:
+  host = 'redis' if os.environ['DEPLOYMENT'] == 'true' else 'localhost'
+except KeyError:
+  host = 'localhost'
+
+from interfaces.helpers import set_init_mock_redis, pop_expired_redis
+from interfaces.helpers import TimeChecker
 from interfaces.nox_analyzer import NOxAnalyzer
 
 
 def mock_worker():
+  r = redis.Redis(host=host, port=6379, db=0)
   set_init_mock_redis()
-  nox = NOxAnalyzer()
+  nox = NOxAnalyzer(r)
+  time_checker_1 = TimeChecker(1)
 
   while True:
-    log = nox.push_mock_data_to_redis()
-    print(log)
-    sleep(1)
+    if time_checker_1.detect_alarm():
+      pop_expired_redis(r, 100)
+      log = nox.push_mock_data_to_redis()
+      time_checker_1.alarm = False
+      print(log)
+      sleep(0.1)
 
 
 if __name__ == '__main__':

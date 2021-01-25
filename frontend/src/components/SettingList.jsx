3f361399d840fa158fbbdbea02c5ac2e2c9a93f0
Yusheng Wu <wuyusheng1023@gmail.com>
2021-01-25 16:11:07 +0200

    fix list key bug, still have 2 warnings

--- frontend/src/components/SettingList.jsx
+++ frontend/src/components/SettingList.jsx
@@ -5,41 +5,40 @@ import EditableTagGroup from './EditableTagGroup'
 
 export default function SettingList() {
   let settingData = [
-    { 'key': '1', 'name': '类型', 'tags': ['校准', '质控', '样品'] },
-    { 'key': '2', 'name': '偏差', 'tags': ['0.1 %', '0.5 %', '1 %'] },
-    { 'key': '3',
-      'name': '气瓶类型',
+    { 'name': '类型', 'tags': ['校准', '质控', '样品'] },
+    { 'name': '偏差', 'tags': ['0.1 %', '0.5 %', '1 %'] },
+    { 'name': '气瓶类型',
       'tags': ['美托 2 升', '美托 4 升', '美托 8 升', '科金 2 升', '科金 4 升', '科金 8 升'] 
     },
-    { 'key': '4', 'name': '气瓶压力', 'tags': ['11.0 MPa', '10.9 MPa', '10.8 MPa', '10.7 MPa'] },
-    { 'key': '5', 'name': '检测人员', 'tags': ['倪才倩,', '范洁'] },
+    { 'name': '气瓶压力', 'tags': ['11.0 MPa', '10.9 MPa', '10.8 MPa', '10.7 MPa'] },
+    { 'name': '检测人员', 'tags': ['倪才倩,', '范洁'] },
   ]
 
   const onUpdate = function(name, tags) {
     settingData = settingData.map(
-        item => item['name'] !== name ? item : {'key': item['key'], name : name, 'tags': tags }
+        item => item['name'] !== name ? item : {name : name, 'tags': tags }
      )
   };
 
   return (
-    [
+    <>
       '保存设置', '恢复默认设置',
       <List
         itemLayout="horizontal"
         dataSource={ settingData }
         renderItem={ item => (
-          <List.Item key={ item['key'] }>
+          <List.Item>
             <List.Item.Meta
-              title={ item['name'] }
+              title={ item.name }
               description=<EditableTagGroup
-                name={ item['name']} 
-                tags={ item['tags']}  
+                name={ item.name } 
+                tags={ item.tags}  
                 onUpdate = { onUpdate }
               />
             />
           </List.Item>
         )}
       />
-    ]
+    </>
   )
 }
